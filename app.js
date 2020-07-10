ba1f11d8718652067f270f6c504ca43088f0432b
tardis-debug <>
2020-07-10 22:14:21 +0200

    hello express
    Connection created to localhost:3000 using express.

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require ('express')
 const mongoDB = require ('mongoDB')
 
-console.log('hello')
+//console.log('hello')
+
+const app = express()
+const port = 3000
+app.get('/', (req, res)  =>
+res.send('hello world!'))
+
+app.listen (port, ()=>
+console.log(`Example app listening at http://localhost:${port}`))
